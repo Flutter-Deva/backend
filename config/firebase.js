8424c06e7672c7a747b11f46f9dffdc08e636430
Vinay Eligeti <102029488+Vinay-Eligeti@users.noreply.github.com>
2025-07-17 15:48:02 +0530

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./notification-d02b2-firebase-adminsdk-fbsvc-7910afc891.json");
+const serviceAccount = require("./notification-d02b2-firebase-adminsdk-fbsvc-25911f99bb.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
